50f86dabc41906b1bebc674b2d0183e1aceb6c62
Victor Lopez <development@greentwip.xyz>
2022-04-13 16:39:09 +0200

    Add luacpp extension

--- thirdparty/lua/luajit/include/lua.hpp
+++ thirdparty/lua/luajit/include/lua.hpp
@@ -1,9 +1,11 @@
 // C++ wrapper for LuaJIT header files.
-
+#ifndef luajit_lua_hpp
+#define luajit_lua_hpp
 extern "C" {
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
 #include "luajit.h"
 }
+#endif
 
